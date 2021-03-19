b4b72acd2b48ae6d8e6a10240dc98436d918a678
Alex Duncan <monkeymedic26@gmail.com>
2021-03-19 15:40:51 -0700

    finished get request query and response.  adjusted schema and reloaded

--- schema.sql
+++ schema.sql
@@ -5,53 +5,67 @@ CREATE DATABASE sdc;
 \c sdc;
 
 CREATE TABLE questions (
-  id SERIAL,
+  question_id SERIAL,
   product_id INT NOT NULL,
-  body TEXT,
-  date_written DATE,
+  question_body TEXT,
+  question_date DATE,
   asker_name VARCHAR(60),
   asker_email VARCHAR(60),
-  reported INT NOT NULL,
-  helpful INT NOT NULL,
-  PRIMARY KEY(id)
+  question_reported BOOLEAN,
+  question_helpfulness INT NOT NULL,
+  PRIMARY KEY(question_id)
 );
 
 CREATE TABLE answers (
-  id SERIAL,
+  answer_id SERIAL,
   question_id INT NOT NULL,
   body TEXT NOT NULL,
-  date_written DATE,
+  date DATE,
   answerer_name VARCHAR(60),
   answerer_email VARCHAR(60),
-  reported INT NOT NULL,
-  helpful INT NOT NULL,
-  PRIMARY KEY(id),
+  reported BOOLEAN,
+  helpfulness INT NOT NULL,
+  PRIMARY KEY(answer_id),
   CONSTRAINT fk_question
     FOREIGN KEY(question_id)
-      REFERENCES questions(id)
+      REFERENCES questions(question_id)
 );
 
 CREATE TABLE answers_photos (
   id SERIAL,
   answer_id INT NOT NULL,
-  url TEXT,
+  url TEXT[],
   PRIMARY KEY(id),
   CONSTRAINT fk_answer
     FOREIGN KEY(answer_id)
-      REFERENCES answers(id)
+      REFERENCES answers(answer_id)
 );
 
-COPY questions(id, product_id, body, date_written, asker_name, asker_email, reported, helpful)
-FROM '/home/alex/Desktop/galvanize/Questions/CSV/answers_photos.csv'
+COPY questions(question_id,
+  product_id,
+  question_body,
+  question_date,
+  asker_name,
+  asker_email,
+  question_reported,
+  question_helpfulness)
+FROM '/home/alex/Desktop/galvanize/Questions/CSV/questions.csv'
 DELIMITER ','
 CSV HEADER;
 
-COPY answers(id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful)
-FROM '/home/alex/Desktop/galvanize/Questions/CSV/answers_photos.csv'
+COPY answers(answer_id,
+  question_id,
+  body,
+  date,
+  answerer_name,
+  answerer_email,
+  reported,
+  helpfulness)
+FROM '/home/alex/Desktop/galvanize/Questions/CSV/answers.csv'
 DELIMITER ','
 CSV HEADER;
 
 COPY answers_photos(id, answer_id, url)
-FROM './CSV/answers_photos.csv'
+FROM '/home/alex/Desktop/galvanize/Questions/CSV/answers_photos.csv'
 DELIMITER ','
 CSV HEADER;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const compression = require('compression');
 const axios = require('axios');
 const config = require('../config.js');
+const db = require('../db/helpers.js');
 
 const PORT = 3000;
 
@@ -19,15 +20,78 @@ const options = {
   },
 };
 
-app.get('/qa/questions/:productId', (req, res) => {
-  const { productId } = req.params;
+app.get('/questions/:params', (req, res) => {
+  const { params } = req.params;
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/qa/questions/?product_id=${params}`, options)
+    .then((data) => res.send(data.data))
+    .catch((err) => console.log('error getting questions', err.response.data));
+});
 
-  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/qa/questions/?product_id=${productId}`, options)
-    .then(({ data }) => {
-      res.status(200).send(data);
-    }).catch((err) => {
-      console.log('there was an error getting questions based on product id', err);
-    });
+app.get('/qa/questions/:productId', (req, res) => {
+  const params = req.params.productId;
+  const query = `SELECT questions.*, answers.*, answers_photos.* FROM questions INNER JOIN answers ON product_id=${params} AND answers.question_id=questions.question_id INNER JOIN answers_photos ON answers_photos.answer_id=answers.answer_id;`;
+  // const query = `
+  // SELECT * FROM questions WHERE product_id=${params};
+  // SELECT questions.product_id, questions.question_id answers.* FROM answers WHERE questions.product_id=${params} AND answers.question_id=questions.question_id;`;
+
+  // const response = {
+  //   product_id: null,
+  //   results: [
+  //     {
+  //       question_id: null,
+  //       question_body: null,
+  //       question_date: null,
+  //       asker_name: null,
+  //       question_helpfulness: null,
+  //       reported: null,
+  //       answers: {
+  //         [answerId]: {
+  //           id: 1259621,
+  //           body: null,
+  //           date: null,
+  //           answerer_name: null,
+  //           helpfulness: null,
+  //           photos: null
+  //         },
+  //       },
+  //     },
+  //   ],
+  // };
+
+  db.query(query, (err, data) => {
+    if (err) {
+      console.log('error in questions get query', err);
+    } else {
+      console.log(data);
+      const response = {
+        product_id: data.rows[0].product_id,
+        results: [],
+      };
+
+      data.rows.map((question) => {
+        const responseObj = {
+          question_id: question.question_id,
+          question_body: question.question_body,
+          question_date: question.question_date,
+          asker_name: question.asker_name,
+          question_helpfulness: question.question_helpfulness,
+          reported: question.question_helpfulness,
+          answers: {
+            [question.answer_id]: {
+              id: question.answer_id,
+              body: question.body,
+              date: question.date,
+              answerer_name: question.answerer_name,
+              helpfulness: question.helpfulness,
+              photos: question.url,
+            },
+          },
+        };
+        response.results.push(responseObj);
+      });
+      res.send(response);
+    }
+  });
 });
 
 app.listen(PORT, () => {
