d3071e680ea5ef5cfcf7cc68d47c53d6557c7f33
Alex Duncan <monkeymedic26@gmail.com>
2021-03-27 10:28:32 -0700

    changed db connection to process.env variables

--- db/helpers.js
+++ db/helpers.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
-const dbp = require('./dbp.js');
-
-const secret = dbp.TOKEN;
 
 const pool = new Pool({
-  user: 'ubuntu',
-  host: '3.142.220.111',
+  user: process.env.USER,
+  host: process.env.HOST,
   database: 'sdc',
-  password: secret,
-  port: 5432,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 });
 
 pool.query('SELECT NOW()', (err) => {
